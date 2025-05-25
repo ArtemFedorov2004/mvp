c0f1080299f09dbcf14fbf127df929aed5cfe9cb
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-05-25 16:21:34 +0300

    added authorization with keycloak, changed IT test

--- customer-app/src/main/java/io/github/artemfedorov2004/customerapp/config/ClientBeans.java
+++ customer-app/src/main/java/io/github/artemfedorov2004/customerapp/config/ClientBeans.java
@@ -1,9 +1,13 @@
 package io.github.artemfedorov2004.customerapp.config;
 
 import io.github.artemfedorov2004.customerapp.client.DefaultProductsRestClient;
+import io.github.artemfedorov2004.customerapp.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -12,9 +16,17 @@ public class ClientBeans {
     @Bean
     public DefaultProductsRestClient productsRestClient(
             @Value("${online-store.services.online-store-service.uri:http://localhost:8080}")
-            String onlineStoreServiceBaseUri) {
+            String onlineStoreServiceBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${online-store.services.online-store-service.registration-id:keycloak}")
+            String registrationId) {
         return new DefaultProductsRestClient(RestClient.builder()
                 .baseUrl(onlineStoreServiceBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
                 .build());
     }
 }
---
+++ online-store-service/src/main/java/io/github/artemfedorov2004/onlinestoreservice/config/SecurityBeans.java
@@ -0,0 +1,29 @@
+package io.github.artemfedorov2004.onlinestoreservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .requestMatchers(HttpMethod.GET).hasAuthority("SCOPE_view_online_store")
+                        .anyRequest().denyAll())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
