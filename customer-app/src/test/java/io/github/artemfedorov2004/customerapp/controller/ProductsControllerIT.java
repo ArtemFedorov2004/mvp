1397ca66e3037bb6d2602475d4ecccaba43a9626
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2025-05-27 16:28:30 +0300

    security rule changed - allow all GET requests add the name of the authorized user to the pages added a link to registration on the pages

--- customer-app/src/test/java/io/github/artemfedorov2004/customerapp/controller/ProductsControllerIT.java
+++ customer-app/src/test/java/io/github/artemfedorov2004/customerapp/controller/ProductsControllerIT.java
@@ -68,17 +68,32 @@ public class ProductsControllerIT {
     }
 
     @Test
-    void getProductList_UserIsNotAuthorized_RedirectsToLoginPage() throws Exception {
+    void getProductList_UserIsNotAuthorized_ReturnsProductsListPage() throws Exception {
         // given
         var requestBuilder = MockMvcRequestBuilders.get("/online-store/products/list");
+
+        WireMock.stubFor(WireMock.get(WireMock.urlPathMatching("/online-store-api/products"))
+                .willReturn(WireMock.ok("""
+                        [
+                            {"id": 1, "title": "Товар №1", "price": 100},
+                            {"id": 2, "title": "Товар №2", "price": 200}
+                        ]""").withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)));
 
         // when
         this.mockMvc.perform(requestBuilder)
                 // then
                 .andDo(print())
                 .andExpectAll(
-                        status().is3xxRedirection()
+                        status().isOk(),
+                        view().name("online-store/products/list"),
+                        model().attribute("products", List.of(
+                                new Product(1L, "Товар №1", new BigDecimal(100)),
+                                new Product(2L, "Товар №2", new BigDecimal(200))
+                        )),
+                        model().attributeDoesNotExist("username")
                 );
+
+        WireMock.verify(WireMock.getRequestedFor(WireMock.urlPathMatching("/online-store-api/products")));
     }
 
     @Test
@@ -140,16 +155,51 @@ public class ProductsControllerIT {
     }
 
     @Test
-    void getProduct_UserIsNotAuthorized_RedirectsToLoginPage() throws Exception {
+    void getProduct_UserIsNotAuthorized_ReturnsProductPage() throws Exception {
         // given
         var requestBuilder = MockMvcRequestBuilders.get("/online-store/products/1");
+
+        WireMock.stubFor(WireMock.get(WireMock.urlPathMatching("/online-store-api/products/1"))
+                .willReturn(WireMock.ok("""
+                        {
+                            "id": 1,
+                            "title": "Товар №1",
+                            "price": 100
+                        }
+                        """).withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)));
 
         // when
         this.mockMvc.perform(requestBuilder)
                 // then
                 .andDo(print())
                 .andExpectAll(
-                        status().is3xxRedirection()
+                        status().isOk(),
+                        view().name("online-store/products/product"),
+                        model().attribute("product",
+                                new Product(1L, "Товар №1", new BigDecimal(100)))
                 );
+
+        WireMock.verify(WireMock.getRequestedFor(WireMock.urlPathMatching("/online-store-api/products/1")));
+    }
+
+    @Test
+    void getProduct_UserIsNotAuthorizedAndProductDoesNotExist_ReturnsError404Page() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.get("/online-store/products/1");
+
+        WireMock.stubFor(WireMock.get(WireMock.urlPathMatching("/online-store-api/products/1"))
+                .willReturn(WireMock.notFound()));
+
+        // when
+        this.mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNotFound(),
+                        view().name("errors/404"),
+                        model().attribute("error", "Товар не найден")
+                );
+
+        WireMock.verify(WireMock.getRequestedFor(WireMock.urlPathMatching("/online-store-api/products/1")));
     }
 }
